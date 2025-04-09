e5afe7ab73c164fbebd3a26d2c872c115312ab6f
src0730 <davemark0730@hotmail.com>
2025-04-09 11:30:07 +0800

    feat: 更新 GridWidgetComponent 以支援過濾功能

--- src/app/components/grid-widget/grid-widget.component.ts
+++ src/app/components/grid-widget/grid-widget.component.ts
@@ -1,6 +1,6 @@
 import { CommonModule } from '@angular/common';
-import { Component, Input } from '@angular/core';
-import { GridOptions } from '../../interfaces/dashboard-config';
+import { Component, Input, OnChanges, SimpleChanges } from '@angular/core';
+import { GridFilterField, GridOptions } from '@interfaces';
 
 @Component({
   selector: 'app-grid-widget',
@@ -9,6 +9,38 @@ import { GridOptions } from '../../interfaces/dashboard-config';
   templateUrl: './grid-widget.component.html',
   styleUrls: ['./grid-widget.component.scss'],
 })
-export class GridWidgetComponent {
+export class GridWidgetComponent implements OnChanges {
+  @Input() filterFields: GridFilterField[] = [];
   @Input({ required: true }) options!: GridOptions;
+
+  data: any[] = [];
+
+  ngOnChanges(changes: SimpleChanges): void {
+    if (changes['options'] || changes['filterFields']) {
+      this.updateData();
+    }
+  }
+
+  private updateData(): void {
+    this.data = this.options?.data || [];
+
+    if (this.filterFields?.length > 0) {
+      this.data = this.data.filter((item) =>
+        this.filterFields.every((filterField) => {
+          const fieldValue: string | number | Date | boolean =
+            item[filterField.fieldId];
+
+          // 判斷值是否為 null 或 undefined，如果是則返回 true，表示不過濾該行。
+          if (fieldValue == null) return true;
+
+          // 判斷型別
+          if (typeof fieldValue === 'string') {
+            return fieldValue.includes(filterField.value.toString());
+          } else {
+            return fieldValue === filterField.value;
+          }
+        })
+      );
+    }
+  }
 }
